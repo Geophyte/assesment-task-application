f2b31ce61567d8d3392bd52cadc75a33794eed3f
Gephyte <idab@vp.pl>
2024-02-07 22:07:12 +0100

    register models in admin.py

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Task, UserProfile, Comment
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Task)
+admin.site.register(UserProfile)
+admin.site.register(Comment)

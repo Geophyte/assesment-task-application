0981939f1d55428ed529f4ee80ad8865fe39e147
Gephyte <idab@vp.pl>
2024-02-08 11:20:20 +0100

    Fix image handling

--- backend/api/serializers/comment.py
+++ backend/api/serializers/comment.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from ..models import Comment
-from .user_profile import UserSerializer
-from .task import TaskSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    user = UserSerializer()
-    task = TaskSerializer()
+    user = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Comment
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['user'] = self.context['request'].user
+        return super().create(validated_data)
--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from .views.category import CategoryViewSet
 from .views.task import TaskViewSet
@@ -20,3 +22,4 @@ urlpatterns = [
 ]
 
 urlpatterns += router.urls
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
